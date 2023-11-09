5bdc50f1b19f76c5a9eace350e6e8eaf84a74ae6
MarceloLima11 <marcelolima152021@gmail.com>
2023-11-09 08:58:33 -0300

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("A sequência de Fibonacci irá até: ");
+
+int length;
+if (!int.TryParse(Console.ReadLine(), out length))
+    throw new Exception("Valor inválido");
+
+int antecessor = 0;
+int atual = 1;
+Console.WriteLine(antecessor);
+Console.WriteLine(atual);
+for (int i = 1; i <= (length - 2); i++)
+{
+    int proximo = antecessor + atual;
+    antecessor = atual;
+    atual = proximo;
+
+    Console.WriteLine(proximo);
+}
